a0e8d2c4d1904cd359bce8fb48a2246892f006b8
Max Spero <mmspero@gmail.com>
2018-11-28 21:48:50 -0800

    update value estimator to take arrays

--- value_estimator.py
+++ value_estimator.py
@@ -20,13 +20,28 @@ class ValueEstimator():
         scaled = self.scaler.transform([state])
         featurized = self.featurizer.transform(scaled)
         return featurized[0]
+
+    def featurize_states(self, state):
+        """
+        Returns the featurized representation for an array of states. Takes in 2D array of states. Returns 2D array of features.
+        """
+        scaled = self.scaler.transform(state)
+        featurized = self.featurizer.transform(scaled)
+        return featurized
     
+    def values(self, s):
+        """
+        Makes value function prediction on an array of states. Takes in a 2D array, returns 1D array list of values.
+        """
+        features = self.featurize_states(s)
+        return self.model.predict(features)
+
     def value(self, s):
         """
-        Makes value function predictions.
+        Makes value function prediction on a single state. Takes in a 2D array (1-length in axis 0) [[state...]]
         """
-        features = self.featurize_state(s)
-        return self.model.predict([features])[0]
+        features = self.featurize_states(s)
+        return self.model.predict(features)[0]
     
     def update(self, s, y):
         """
